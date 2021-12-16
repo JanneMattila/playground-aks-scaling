7de21f733e2554a91c8d9b3c5a303f04fc7f7f63
Janne Mattila <janne.mattila@hotmail.fi>
2021-12-16 14:17:45 +0200

    Add placeholder files for scale demos

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# playground-aks-scaling
+# Playground AKS Scaling
+
 Playground for AKS and scaling
+
+## Links
+
+[Stop and Start an Azure Kubernetes Service (AKS) cluster](https://docs.microsoft.com/en-us/azure/aks/start-stop-cluster?tabs=azure-cli)
+[Use Scale-down Mode to delete/deallocate nodes in Azure Kubernetes Service (AKS) (preview)](https://docs.microsoft.com/en-us/azure/aks/scale-down-mode)
---
+++ setup.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+# All the variables for the deployment
+subscriptionName="AzureDev"
+aadAdminGroupContains="janne''s"
+
+aksName="myaksscaling"
+workspaceName="myscalingworkspace"
+vnetName="myaksscaling-vnet"
+subnetAks="AksSubnet"
+identityName="myaksscaling"
+resourceGroupName="rg-myaksscaling"
+location="westeurope"
+
+# Login and set correct context
+az login -o table
+az account set --subscription $subscriptionName -o table
+
+subscriptionID=$(az account show -o tsv --query id)
+az group create -l $location -n $resourceGroupName -o table
+
+# Prepare extensions and providers
+az extension add --upgrade --yes --name aks-preview
+
+# Enable features
+az feature register --namespace "Microsoft.ContainerService" --name "AKS-ScaleDownModePreview"
+az feature register --namespace "Microsoft.ContainerService" --name "PodSubnetPreview"
+az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/AKS-ScaleDownModePreview')].{Name:name,State:properties.state}"
+az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/PodSubnetPreview')].{Name:name,State:properties.state}"
+az provider register --namespace Microsoft.ContainerService
+
+# Remove extension in case conflicting previews
+az extension remove --name aks-preview
+
+aadAdmingGroup=$(az ad group list --display-name $aadAdminGroupContains --query [].objectId -o tsv)
+echo $aadAdmingGroup
+
+workspaceid=$(az monitor log-analytics workspace create -g $resourceGroupName -n $workspaceName --query id -o tsv)
+echo $workspaceid
+
+vnetid=$(az network vnet create -g $resourceGroupName --name $vnetName \
+  --address-prefix 10.0.0.0/8 \
+  --query newVNet.id -o tsv)
+echo $vnetid
+
+subnetaksid=$(az network vnet subnet create -g $resourceGroupName --vnet-name $vnetName \
+  --name $subnetAks --address-prefixes 10.2.0.0/24 \
+  --query id -o tsv)
+echo $subnetaksid
+
+identityid=$(az identity create --name $identityName --resource-group $resourceGroupName --query id -o tsv)
+echo $identityid
+
+az aks get-versions -l $location -o table
+
+# Note: for public cluster you need to authorize your ip to use api
+myip=$(curl --no-progress-meter https://api.ipify.org)
+echo $myip
+
+# Note about private clusters:
+# https://docs.microsoft.com/en-us/azure/aks/private-clusters
+
+# For private cluster add these:
+#  --enable-private-cluster
+#  --private-dns-zone None
+
+# Choose correct OS disk
+osDisk="Ephemeral"
+osDisk="Managed"
+
+az aks create -g $resourceGroupName -n $aksName \
+ --max-pods 50 --network-plugin azure \
+ --node-count 1 --enable-cluster-autoscaler --min-count 1 --max-count 3 \
+ --node-osdisk-type Ephemeral \
+ --node-vm-size Standard_D4ds_v4 \
+ --kubernetes-version 1.21.2 \
+ --enable-addons monitoring \
+ --enable-aad \
+ --enable-managed-identity \
+ --disable-local-accounts \
+ --aad-admin-group-object-ids $aadAdmingGroup \
+ --workspace-resource-id $workspaceid \
+ --load-balancer-sku standard \
+ --vnet-subnet-id $subnetaksid \
+ --assign-identity $identityid \
+ --api-server-authorized-ip-ranges $myip \
+ -o table 
+
+###################################################################
+# Enable current ip
+az aks update -g $resourceGroupName -n $aksName --api-server-authorized-ip-ranges $myip
+
+# Clear all authorized ip ranges
+az aks update -g $resourceGroupName -n $aksName --api-server-authorized-ip-ranges ""
+###################################################################
+
+sudo az aks install-cli
+
+az aks get-credentials -n $aksName -g $resourceGroupName --overwrite-existing
+
+kubectl get nodes
+
+# Create namespace
+kubectl apply -f namespace.yaml
+kubectl apply -f demos
+
+kubectl get deployment -n demos
+kubectl describe deployment -n demos
+
+kubectl get pod -n demos
+pod1=$(kubectl get pod -n demos -o name | head -n 1)
+echo $pod1
+
+kubectl describe $pod1 -n demos
+
+kubectl get service -n demos
+
+ingressip=$(kubectl get service -n demos -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
+echo $ingressip
+
+curl $ingressip
+
+# Wipe out the resources
+az group delete --name $resourceGroupName -y
